a8798c7c8e36e99869ac61f77834cf93e17ed7b5
Wisang Drillian Geni <gwisang45@gmail.com>
2024-10-23 17:51:57 +0700

    commenting passport auth

--- src/app.mjs
+++ src/app.mjs
@@ -3,6 +3,7 @@ import routes from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
 import session from "express-session";
 import passport from "passport";
+import "./strategies/local-strategy.mjs";
 
 const app = express();
 
@@ -21,7 +22,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(routes);
 
-const test = "testing";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
@@ -40,3 +40,44 @@ app.get("/", (req, res) => {
       "hello, you can change the query to see other data, /api/users or /api/products"
     );
 });
+
+// app.post("/api/auth", passport.authenticate(local), (req, res) => {});
+
+// app.post("/api/auth", (req, res) => {
+//   const {
+//     body: { username, password },
+//   } = req;
+//   const findUser = mockUsers.find((user) => user.username === username);
+//   if (!findUser || findUser.password !== password)
+//     return res.status(401).send({ msg: "Bad Credentials" });
+
+//   req.session.user = findUser;
+//   return res.status(200).send(findUser);
+// });
+
+// app.get("/api/auth/status", (req, res) => {
+//   req.sessionStore.get(req.sessionID, (err, session) => {
+//     console.log(session);
+//   });
+//   return req.session.user
+//     ? res.status(200).send(req.session.user)
+//     : res.status(401).send({ msg: "Not Authenticated" });
+// });
+
+// app.post("/api/cart", (req, res) => {
+//   if (!req.session.user) return res.sendStatus(401);
+//   const { body: item } = req;
+//   const { cart } = req.session;
+
+//   if (cart) {
+//     cart.push(item);
+//   } else {
+//     req.session.cart = [item];
+//   }
+//   return res.status(201).send(item);
+// });
+
+// app.get("/api/cart", (req, res) => {
+//   if (!req.session.user) return res.sendStatus(401);
+//   return res.send(req.session.cart ?? []);
+// });
--- src/strategies/local-strategy.mjs
+++ src/strategies/local-strategy.mjs
@@ -1,2 +1,28 @@
 import passport from "passport";
 import { Strategy } from "passport-local";
+import { mockUsers } from "../utils/constants.mjs";
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done)=> {
+  
+});
+
+export default passport.use(
+  new Strategy((username, password, done) => {
+    console.log(`Username: ${username} `);
+    console.log(`Password: ${password} `);
+
+    try {
+      const findUser = mockUsers.find((user) => user.username === username);
+      if (!findUser) throw new Error("user not found");
+      if (findUser.password !== password)
+        throw new Error("invalid credentials");
+      done(null, findUser);
+    } catch (err) {
+      done(err, null);
+    }
+  })
+);
